f683f5cc9ee2c8a53bef633d37d2e7b27fa5a84c
SkirdaMK <skirda.michael@gmail.com>
2023-09-14 12:35:35 +0300

    Set added

--- RenSharp/Models/Commands/Message.cs
+++ RenSharp/Models/Commands/Message.cs
@@ -8,6 +8,7 @@ namespace RenSharp.Models.Commands
 {
     public class Message : Command
 	{
+		private string RawLine { get; set; }
 		public string Speech { get; set; }
 		public string Character { get; set; }
 		public Attributes Attributes { get; set; }
@@ -15,12 +16,15 @@ namespace RenSharp.Models.Commands
 		public Message(string speech, string character, IEnumerable<string> attributes = null)
 		{
 			Speech = speech;
+			RawLine = speech;
 			Character = character;
 			Attributes = new Attributes(attributes);
 		}
 
 		internal override void Execute(RenSharpCore renSharpCore, RenSharpContext context)
 		{
+			Speech = context.ReplaceVars(RawLine);
+
 			Attributes attributes = renSharpCore.GetCharacterAttributes(Character);
 			Attributes.AddAttributes(attributes);
 
--- RenSharp/Models/Commands/Set.cs
+++ RenSharp/Models/Commands/Set.cs
@@ -8,17 +8,18 @@ namespace RenSharp.Models.Commands
 	public class Set : Command
 	{
 		public string Name { get; set; }
-		public string Value { get; set; }
+		public string Expression { get; set; }
 
-		public Set(string name, string value)
+		public Set(string name, string expression)
 		{
 			Name = name;
-			Value = value;
+			Expression = expression;
 		}
 
 		internal override void Execute(RenSharpCore renSharpCore, RenSharpContext context)
 		{
-			context.Variables[Name] = Value;
+			string result = context.ExecuteExpression<object>(Expression).ToString();
+			context.Variables[Name] = result;
 		}
 	}
 }
