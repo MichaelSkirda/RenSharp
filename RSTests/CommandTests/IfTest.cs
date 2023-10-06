0ca937fefa131ced51fd2b2af3c6351d04d10980
SkirdaMK <skirda.michael@gmail.com>
2023-10-06 09:56:12 +0300

    Tests files moved

---
+++ RSTests/CommandTests/IfTest.cs
@@ -0,0 +1,24 @@
+namespace RSTests.CommandTests
+{
+    public class IfTest : TestBase
+    {
+        public IfTest() : base() { }
+
+        [Theory]
+        [InlineData("./CommandTests/IfTests/5_biggerThan_2_initPy.csren")]
+        [InlineData("./CommandTests/IfTests/5_biggerThan_2_initPyBlock.csren")]
+        [InlineData("./CommandTests/IfTests/5_biggerThan_2_initPyol.csren")]
+        [InlineData("./CommandTests/IfTests/5_biggerThan_2_pyol.csren")]
+        [InlineData("./CommandTests/IfTests/5_biggerThan_2_rsset.csren")]
+        [InlineData("./CommandTests/IfTests/5_biggerThan_2_rssetinit.csren")]
+        public void Five_BiggerThan_Two(string path)
+        {
+            ExecuteUntilExit(path);
+            Message message = Messages.First();
+
+            Assert.Single(Messages);
+            Assert.Equal("nobody", message.Character);
+            Assert.Equal("x bigger than 5", message.Speech);
+        }
+    }
+}
